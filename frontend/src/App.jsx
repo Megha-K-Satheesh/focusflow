0225ec6677b7eae350f3d066c34f9f338800341f
Megha-K-Satheesh <megha.k.satheesh@gmail.com>
2026-07-04 18:13:15 +0530

    feat: update application routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,4 @@
-// import { Route, Routes } from "react-router-dom";
-// import { ToastContainer } from "react-toastify";
-// import './App.css';
-// import ForgotPassword from "./pages/auth/ForgotPassword";
-// import Login from "./pages/auth/Login";
-// import Register from './pages/auth/Register';
-// import ResetPassword from "./pages/auth/ResetPassword";
-// import VerifyOtp from "./pages/auth/VerifyOtp";
-// import ResetPasswordOtp from "./pages/auth/VerifyResetPasswordOtp";
-// import CreateStudyPlan from "./pages/user/CreateStudyPlan";
-// import Home from "./pages/user/Home";
-// import InterviewFeedback from "./pages/user/InterviewFeedback";
-// import InterviewHistory from "./pages/user/InterviewHistory";
-// import InterviewSession from "./pages/user/InterviewSession";
-// import InterviewStart from "./pages/user/InterviewStart";
-// import LandingPage from "./pages/user/LandingPage";
-// import ViewStudyPlan from "./pages/user/ViewStudyPlan";
-// import AboutPage from "./pages/user/AboutPage";
-// function App() {
 
-
-//   return (
-//     <>
-//      <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/register" element={<Register />} />
-//       <Route path="/verify-otp" element={<VerifyOtp />} />
-//       <Route path="/login" element={<Login/>} />
-//       <Route path="/forgot-password" element={<ForgotPassword/>} />
-//       <Route path="/verify-reset-password-otp" element={<ResetPasswordOtp/>} />
-//       <Route path="/reset-password" element={<ResetPassword/>} />
-//       <Route path="/create-study-plan" element={<CreateStudyPlan/>} />
-//       <Route path="/view-study-plan"  element={<ViewStudyPlan/>} />
-//       <Route path="/interview-start"  element={<InterviewStart/>} />
-//       <Route  path="/interview-session/:interviewId"  element={<InterviewSession/>} />
-//      <Route path="/interview/:interviewId/feedback" element={<InterviewFeedback />}/>
-    
-//      <Route path="/interview-history" element = {<InterviewHistory/>}/>
-//      <Route path="/landing-page" element = {<LandingPage/>}/>
-//      <Route path="/about" element = {<AboutPage />}/>
-
-
-// </Routes>
-//           <ToastContainer />
-//     </>
-//   )
-// }
-
-// export default App
 
 
 
@@ -103,7 +55,7 @@ function App() {
 
           {/* PublicRoute */}
           <Route element={<PublicRoute />}>
-          <Route path="/landing-page" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
       
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
@@ -121,7 +73,7 @@ function App() {
 
           {/* Protected Pages */}
           <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Home />} />
 
             <Route
               path="/create-study-plan"
